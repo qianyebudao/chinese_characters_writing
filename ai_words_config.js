7ebd96e15d8ec24a9653811e3a5d04a8ccd60291
qianyebudao <qianyebudao@users.noreply.github.com>
2026-01-09 17:37:43 +0800

    Add files via upload

--- ai_words_config.js
+++ ai_words_config.js
@@ -1,6 +1,7 @@
 window.AI_WORDS_CONFIG = {
     baseUrl: 'https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions',
-    model: 'qwen3-vl-flash',
-    apiKey: ''
+    model: 'qwen-flash',
+    apiKey: 'sk-eea792e96e934403922ef19c2b0b4e56',
+    enable_thinking: true,
+    thinking_budget: 2048
 };
-
